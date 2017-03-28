95e6ca9c540b2a960558adfe00f7f2034fe385bb
Cyril RAT <cyril.rat@gmail.com>
2017-03-28 16:03:24 +0200

    Update UI

---
+++ ABI/FormClient/frmDspClient.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ABI
+{
+	partial class frmDspClient
+	{
+	        /// <summary>
+	        /// Variable nécessaire au concepteur.
+	        /// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+	        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.SuspendLayout();
+            // 
+            // cbxType
+            // 
+            this.cbxType.Enabled = false;
+            // 
+            // txtRaisonSocial
+            // 
+            this.txtRaisonSocial.Enabled = false;
+            // 
+            // cbxActivity
+            // 
+            this.cbxActivity.Enabled = false;
+            // 
+            // cbxNature
+            // 
+            this.cbxNature.Enabled = false;
+            // 
+            // txtCA
+            // 
+            this.txtCA.Enabled = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Enabled = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            // 
+            // txtCodePostal
+            // 
+            this.txtCodePostal.Enabled = false;
+            // 
+            // frmDspClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(730, 696);
+            this.Name = "frmDspClient";
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
