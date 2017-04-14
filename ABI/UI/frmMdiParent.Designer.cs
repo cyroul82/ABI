b3d8096a23c07e8a45d25b79c45d768288224d87
Cyril Rat <cyril.rat@gmail.com>
2017-04-14 05:44:09 +0200

    set default size and minimum size frmMdiParent

--- ABI/UI/frmMdiParent.Designer.cs
+++ ABI/UI/frmMdiParent.Designer.cs
@@ -42,7 +42,7 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.MdiWindowListItem = this.fenêtreToolStripMenuItem;
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(769, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(944, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -63,10 +63,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(769, 404);
+            this.ClientSize = new System.Drawing.Size(944, 561);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(960, 600);
             this.Name = "FormMain";
             this.Text = "ABI";
             this.menuStrip1.ResumeLayout(false);
